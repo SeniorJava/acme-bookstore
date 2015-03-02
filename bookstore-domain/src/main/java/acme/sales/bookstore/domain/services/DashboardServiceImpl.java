28b325c90dbd49e4df178e620ceb3686529201fb
tomcat77 <T3mXfPXOIkXv>
2015-03-02 18:32:15 +0300

    Настроено расписание построения статистики для менеджера, контексты разнесены по разным файлам

--- bookstore-domain/src/main/java/acme/sales/bookstore/domain/services/DashboardServiceImpl.java
+++ bookstore-domain/src/main/java/acme/sales/bookstore/domain/services/DashboardServiceImpl.java
@@ -1,6 +1,8 @@
 package acme.sales.bookstore.domain.services;
 
 import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
@@ -29,10 +31,15 @@ public class DashboardServiceImpl implements DashboardService {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    private Logger logger = LoggerFactory.getLogger("DashboardService");
+
     @Override
     @Transactional
     public void collectStats() {
+        logger.info("Collecting dashboard statistic...");
+
         final Date today = DateUtils.truncate(new Date(), Calendar.DAY_OF_MONTH);
+
         final Integer orderQty = jdbcTemplate.queryForObject("SELECT count(1) FROM book_order WHERE order_date = ?",
                 Integer.class, today);
         final Double totalSum = jdbcTemplate.queryForObject(
@@ -41,36 +48,25 @@ public class DashboardServiceImpl implements DashboardService {
 
         Integer todayStatCount = jdbcTemplate.queryForObject("SELECT count(1) FROM dashboard_stats WHERE stat_date=?",
                 Integer.class, today);
-
-        PreparedStatementCallback<Object> setParamsCallback = new PreparedStatementCallback<Object>() {
-
-            @Override
-            public Object doInPreparedStatement(PreparedStatement pStatement) throws SQLException, DataAccessException {
-                pStatement.setInt(1, orderQty);
-                pStatement.setDouble(2, totalSum);
-                pStatement.setDate(3, new java.sql.Date(today.getTime()));
-
-                pStatement.executeUpdate();
-
-                return null;
-            }
-        };
-
         boolean alreadyCollected = todayStatCount > 0;
+
+        PreparedStatementCallback<Object> updateCallback = new UpdateStatsCallback(orderQty, totalSum, today);
 
         if (alreadyCollected) {
             jdbcTemplate.execute("UPDATE dashboard_stats SET orders_qty=?, total_sum=? WHERE stat_date=?",
-                    setParamsCallback);
+                    updateCallback);
         } else {
             jdbcTemplate.execute("INSERT INTO dashboard_stats(orders_qty, total_sum, stat_date) VALUES (?,?,?)",
-                    setParamsCallback);
+                    updateCallback);
         }
+
+        logger.info("Dashboard statistic collected");
     }
 
     @Override
     public DashboardStats getStats(Date date) {
         List<DashboardStats> statsList = jdbcTemplate.query(
-                "select * from dashboard_stats where stat_date=?",
+                "SELECT * FROM dashboard_stats WHERE stat_date=?",
                 DASHBOARD_STATS_ROW_MAPPER, date);
 
         return statsList.isEmpty() ? null : statsList.get(0);
@@ -85,6 +81,30 @@ public class DashboardServiceImpl implements DashboardService {
             stats.setStatsDate(rs.getDate("stat_date"));
 
             return stats;
+        }
+    }
+
+    private static class UpdateStatsCallback implements PreparedStatementCallback<Object> {
+
+        private final Integer orderQty;
+        private final Double totalSum;
+        private final Date today;
+
+        public UpdateStatsCallback(Integer orderQty, Double totalSum, Date today) {
+            this.orderQty = orderQty;
+            this.totalSum = totalSum;
+            this.today = today;
+        }
+
+        @Override
+        public Object doInPreparedStatement(PreparedStatement pStatement) throws SQLException, DataAccessException {
+            pStatement.setInt(1, orderQty != null ? orderQty : 0);
+            pStatement.setDouble(2, totalSum != null ? totalSum : 0.0);
+            pStatement.setDate(3, new java.sql.Date(today.getTime()));
+
+            pStatement.executeUpdate();
+
+            return null;
         }
     }
 }
